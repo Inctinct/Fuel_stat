e8a8b77441cf305e4ae9f7e94e77d4bd00f2a43f
Inctinct <dmitriy.makeychik@mail.ru>
2023-04-03 15:40:14 +0300

    add tasks for gps imitation and random imitation func

--- fuel/statistic/tasks.py
+++ fuel/statistic/tasks.py
@@ -1,12 +1,13 @@
 from celery import shared_task
 from django.utils.http import urlsafe_base64_encode
 
-from .models import RegistredUser
+from .models import RegistredUser, Car, GpsImitation, CheckFuel
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from .tokens import account_activation_token
 from django.core.mail import send_mail
+from .imitation import fuel_imitation, odometer_imitation, refueling_imitation
 
 
 @shared_task()
@@ -25,4 +26,26 @@ def send_activation_mail(user_id, domain):
               from_email=settings.EMAIL_HOST_USER)
 
     return is_sent
+
+
+@shared_task()
+def gps_imitation():
+    cars = Car.objects.all()
+
+    for car in cars:
+        gps = GpsImitation.objects.filter(car=car).order_by('-id')[:1]
+        GpsImitation.objects.create(car=car, fuel_sensor=fuel_imitation(),
+                                    odometer=odometer_imitation(gps.odometer))
+
+
+@shared_task()
+def check_imitation():
+    cars = Car.objects.all()
+
+    for car in cars:
+        test = CheckFuel.objects.first()
+        CheckFuel.objects.create(car=car, refueling=test.refueling,
+                                 fuel=test.fuel, number_of_liters=refueling_imitation())
+
+
 
